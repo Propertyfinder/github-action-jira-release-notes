7e273c97b2883835e6faf2704d108c644c1b1bfc
Murat <murat.varol@propertyfinder.ae>
2024-11-20 21:10:23 +0400

    NA-3905: Added parser for issues

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,50 @@ import * as core from "@actions/core";
 import request from "request-promise";
 import * as _ from 'lodash'
 
+
+const jsonString = `
+{
+    "issues": [
+        {
+            "id": "149645",
+            "key": "NA-3797",
+            "fields": {
+                "summary": "Reduce Main Thread Load in the PLP if exist",
+                "project": {
+                    "name": "Native Apps"
+                },
+                "components": [
+                    {
+                        "name": "Android"
+                    }
+                ],
+                "assignee": null
+            }
+        },
+        {
+            "id": "149128",
+            "key": "NA-3775",
+            "fields": {
+                "summary": "Update new rega fields alignments",
+                "project": {
+                    "name": "Native Apps"
+                },
+                "components": [
+                    {
+                        "name": "Android"
+                    },
+                    {
+                        "name": "Squad: Search Experience"
+                    }
+                ],
+                "assignee": {
+                    "displayName": "Murat Varol"
+                }
+            }
+        }
+    ]
+}`;
+
 (async () => {
     const domain = core.getInput("domain") || "pfinder";
     const projects = core.getInput("projects") || "NA, CX, GROW, NP";
@@ -12,12 +56,13 @@ import * as _ from 'lodash'
     const baseUrl = `https://pfinder.atlassian.net/rest/api/3/search/jql`
 
     try {
-        const releaseData = await getIssues(baseUrl, projects, version, token)
-        const releaseUrl = getJiraQueryUrl(domain, projects, version)
-        core.setOutput("release_notes_url", releaseUrl);
+        const releaseData = await getIssues(baseUrl, projects, version, token);
+        const releaseUrl = getJiraQueryUrl(domain, projects, version);
+        const parsedJson : IssuesData = JSON.parse(jsonString)
+        const releaseNotes = convertToGitHubReleaseGroupedByProject(parsedJson, version, releaseUrl);
+        console.log("\n\nreleaseNotes:", releaseNotes);
         core.setOutput("release_notes", `${releaseUrl}\n${JSON.stringify(releaseData, null, 2)}`);
     } catch (error: any) {
-        core.setOutput("I failed:", error.message)
         core.setFailed(error.message);
     }
 
@@ -46,68 +91,69 @@ async function getIssues(baseUrl: string, projects: string, version: string, tok
 
 function getJiraQueryUrl(domain: string, projects: string, version: string): string {
     const firstProject = projects.split(",")[0].trim()
-    const url = `https://${domain}.atlassian.net/jira/software/c/projects${firstProject}/issues`
+    const url = `https://${domain}.atlassian.net/jira/software/c/projects/${firstProject}/issues`
     const query = `project IN (${projects}) AND component = ${version}`
     const encodedQuery = encodeURIComponent(query);
+
+    // working
+    // [Jira - PF Android](https://pfinder.atlassian.net/jira/software/c/projects/NA/issues?jql=project%20IN%20(NA%2C%20CX%2C%20GROW%2C%20NP)%20AND%20component%20%3D%20Android)
     return `## [Jira - PF ${version}](${url}?jql=${encodedQuery})`
 }
 
-function getReleaseNotes(response: string): string{
-    return ""
-}
+function convertToGitHubReleaseGroupedByProject(data: IssuesData, version: string, releaseUrl: string): string {
+    const releaseTitle = `[Pf Android - ${version}](${releaseUrl})`;
 
+    // Group issues by project name
+    const issuesByProject: Record<string, Issue[]> = data.issues.reduce((group, issue) => {
+        const projectName = issue.fields.project.name;
+        if (!group[projectName]) {
+            group[projectName] = [];
+        }
+        group[projectName].push(issue);
+        return group;
+    }, {} as Record<string, Issue[]>);
 
-function getNote(response: any, baseUrl: string): string {
-    const groupedIssues = getGroupedIssues(response.issues, baseUrl)
-    if (groupedIssues.length == 0) {
-        return ""
-    }
+    // Generate release notes grouped by project
+    const releaseBody = Object.entries(issuesByProject)
+        .map(([projectName, issues]) => {
+            const projectSection = `## ${projectName}\n`;
+            const issuesList = issues
+                .map(issue => {
+                    const assignee = issue.fields.assignee
+                        ? issue.fields.assignee.displayName
+                        : "Unassigned";
+                    const components = issue.fields.components.map(c => c.name).join(", ");
+                    return `[${issue.key}](https://pfinder.atlassian.net/browse/${issue.key}) ${issue.fields.summary} - ${assignee}`;
 
-    return groupedIssues.reduce((result: string, groupedIssue: GroupedIssue) => {
-        result += "\n\n"
-        result += `### ${groupedIssue.type}`
-        groupedIssue.issues.forEach(issue => {
-            result += `\n\n[${issue.key}](${issue.url}) ${issue.summary}`
+                })
+                .join("\n\n");
+            return projectSection + issuesList;
         })
-        return result
-    }, "")
+        .join("\n\n");
+
+    return `# ${releaseTitle}\n\n${releaseBody}`;
 }
 
-function getGroupedIssues(rawValue: any, baseUrl: string): GroupedIssue[] {
-    const issues: Issue[] = rawValue.map((value: any) => {
-
-        const key = value.key
-        const url = baseUrl + "browse/" + key
-
-        const fields = value.fields
-        const summary = (fields.summary as string).replace(/"/g, "\`").replace(/'/g, "\`")
-        const type = fields.issuetype.name
-
-        return new Issue(key, summary, type, url)
-    }).sort((a: Issue, b: Issue) => {
-        return a.type > b.type ? 1 : -1
-    })
-
-    const groupedResult: _.Dictionary<Issue[]> = _.groupBy<Issue>(issues, function (issue: Issue) {
-        return issue.type
-    })
-    return _.map(groupedResult, (items: Issue[], key: string) => {
-        return new GroupedIssue(key, items);
-    });
+interface Issue {
+    id: string;
+    key: string;
+    fields: {
+        summary: string;
+        assignee: {
+            displayName: string;
+            emailAddress: string;
+        };
+        project: {
+            name: string;
+        };
+        components: {
+            name: string;
+        }[];
+    };
 }
 
-class Issue {
-    key: string
-    summary: string
-    type: string
-    url: string
-
-    constructor(key: string, summary: string, type: string, url: string) {
-        this.key = key;
-        this.summary = summary;
-        this.type = type;
-        this.url = url;
-    }
+interface IssuesData {
+    issues: Issue[];
 }
 
 class GroupedIssue {
